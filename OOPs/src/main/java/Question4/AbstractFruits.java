8eb158d78367bde075a5726f0aa2a50fe00993f6
sheetalKPI <114053895+sheetalKPI@users.noreply.github.com>
2022-10-03 05:11:01 -0700

    commit

---
+++ OOPs/src/main/java/Question4/AbstractFruits.java
@@ -0,0 +1,20 @@
+package Question4;
+
+public class AbstractFruits {
+
+	public static void main(String[] args) {
+	Apple a = new Apple();
+	Grapes g = new Grapes();
+	Orange o = new Orange();
+	Pineapple p = new Pineapple();
+	a.taste();
+	a.color();
+    g.taste();
+    g.color();
+    o.taste();
+    o.color();
+    p.taste();
+    p.color();
+	}
+
+}
